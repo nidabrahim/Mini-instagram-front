c1a9ebeb94c6a643a16c494639513dab63927fc6
Youssef Nidabrahim <y.nidabrahim@piman2-0.fr>
2019-03-01 22:39:14 +0100

    gh pages

---
+++ build/precache-manifest.8827b965a1268a0f7f47d0b0b798c4a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb6c89eef795749dc218cf33cae3c279",
+    "url": "/static/media/logo.cb6c89ee.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "10345815a25056216a44",
+    "url": "/static/js/main.10345815.chunk.js"
+  },
+  {
+    "revision": "5b3a609cfcd501c0ee77",
+    "url": "/static/js/1.5b3a609c.chunk.js"
+  },
+  {
+    "revision": "10345815a25056216a44",
+    "url": "/static/css/main.839c718a.chunk.css"
+  },
+  {
+    "revision": "5b3a609cfcd501c0ee77",
+    "url": "/static/css/1.a12fb3a3.chunk.css"
+  },
+  {
+    "revision": "a95edd3b387bd2c35df1ea16062a62aa",
+    "url": "/index.html"
+  }
+];
