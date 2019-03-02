7199b3c7f7557147d7a007cbb2c5852822b14fc9
Youssef Nidabrahim <y.nidabrahim@piman2-0.fr>
2019-03-02 01:51:51 +0100

    Update http api url to https

--- src/api.js
+++ src/api.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export default axios.create({
     // baseURL: `http://localhost:8080/api/`
-    baseURL: `http://nodejs-mongo-persistent-instazz.7e14.starter-us-west-2.openshiftapps.com/api/`
+    baseURL: `https://nodejs-mongo-persistent-instazz.7e14.starter-us-west-2.openshiftapps.com/api/`
     
 });
