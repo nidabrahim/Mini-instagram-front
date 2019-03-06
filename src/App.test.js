b024c11a92a44f8ddc8f694cc53e09bdb3123961
Youssef Nidabrahim <y.nidabrahim@piman2-0.fr>
2019-03-06 22:28:10 +0100

    clean

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import LangingPage from './landingPage';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+   const div = document.createElement('div');
+  // ReactDOM.render(<Router><LangingPage /></Router>, document.getElementById('root'));
+  // ReactDOM.unmountComponentAtNode(div);
 });
